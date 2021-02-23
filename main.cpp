363bd240c9f7606acc1c89c857577976d0e35f50
Adam Owen <ahowenjr@alaska.edu>
2021-02-22 21:42:53 -0900

    forgot to commit. tons of progress made. functions readLines and printTokens work amazingly well. im really happy about that. i need to time it though.

--- main.cpp
+++ main.cpp
@@ -13,12 +13,14 @@ using std::ostream;
 #include <sstream>
 using std::istringstream;
 #include <iterator>
+#include <iomanip>
 
 struct TokenAndPosition{
     string _token;
     int _line;
     unsigned int _column;
 };
+
 vector<string> lineToTokens(const string &line){
     vector<string> tokenizedString;
     auto iss = istringstream {line};
@@ -28,29 +30,51 @@ vector<string> lineToTokens(const string &line){
     }
     return tokenizedString;
 };
-vector<TokenAndPosition> readLines(istream &is);
-void printTokens(ostream &os, const vector<TokenAndPosition> &tokens);
 
-int main() {
-    ifstream myFile("jane eyre.txt", std::ios::binary | std::ios::in);
-    vector <string> dataVector;
+vector<TokenAndPosition> readLines(istream &is){
+    vector<TokenAndPosition> readLine;
+    TokenAndPosition readLineElement;
     string lineInFile;
+    int lineCount = 0;
     while (true) {
-        if (!myFile) {
-            if (myFile.eof()) {
+        if (!is) {
+            if (is.eof()) {
                 cout << "reached end of file" << endl;
             }
             break;
         }
-        getline(myFile, lineInFile);
-        lineToTokens(lineInFile);
-
+        getline(is, lineInFile);
+        if (!(lineInFile.empty())) {
+            auto tempVec = lineToTokens(lineInFile);
+            for (auto i = 0; i < tempVec.size(); ++i) {
+                string str = tempVec[i];
+                int found = lineInFile.find(tempVec[i]);
+                readLineElement._line = lineCount;
+                readLineElement._column = found;
+                readLineElement._token = tempVec[i];
+                readLine.push_back(readLineElement);
+            }
+            lineCount++;
+        }
     }
+    return readLine;
+};
 
-    //vector <TokenAndPosition> readLines;
+void printTokens(ostream &os, const vector<TokenAndPosition> &tokens){
+    for (auto i = 0; i < tokens.size(); ++i){
+        std::ostringstream ss1;
+        int lineWidth = std::to_string(tokens[i]._line).length();
+        int columnWidth = std::to_string(tokens[i]._column).length();
+        ss1 << "Line" << std::setw(lineWidth + 2) << tokens [i]._line
+        << ", Column" << std::setw(columnWidth + 2) << tokens[i]._column
+        << ": " << std::setw(2) << tokens[i]._token;
+        os << ss1.str() << endl;
+    }
+};
 
-
-
+int main() {
+    ifstream myFile("jane eyre.txt", std::ios::binary | std::ios::in);
+    printTokens(cout,readLines(myFile));
 
     return 0;
 }
